f9b63dfeb5c121de2b360c954fb5ff069d928066
PuruMohite <107110840+PuruMohite@users.noreply.github.com>
2026-05-08 19:34:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
   // your code here
+  if(s.length == 0 || !s.includes(" "))
+    return s;
+
+  let trimmedStr = s.trim();
+  return trimmedStr.split(" ")[0];
+
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
